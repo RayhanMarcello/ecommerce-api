9b64895ea13dc8c4294847cd3f965543b8c60d2f
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-02-21 20:56:32 +0700

    create order controller

---
+++ controller/orderController.go
@@ -0,0 +1,55 @@
+package controller
+
+import (
+	"golang-emarket/dto"
+	"golang-emarket/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderControllers struct {
+	orderService service.OrderService
+}
+
+func NewOrderController(orderService service.OrderService) *OrderControllers {
+	return &OrderControllers{orderService: orderService}
+}
+
+func (h *OrderControllers) CreateOrder(c *gin.Context) {
+	var req dto.CreateOrderRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"err": err,
+		})
+		return
+	}
+
+	order, err := h.orderService.CreateOrder(req)
+	if err != nil {
+		c.JSON(404, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, order)
+
+}
+
+func (h *OrderControllers) GetByID(c *gin.Context) {
+	id := c.Param("id")
+	idParse, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"message": err})
+		return
+	}
+
+	order, err := h.orderService.GetOrder(uint(idParse))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "order not found"})
+		return
+	}
+	c.JSON(200, order)
+}
